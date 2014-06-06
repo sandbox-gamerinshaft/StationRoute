5fe95ae99c48ab61b80291fddebfead60dbccca0
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-06-07 02:04:10 +0900

    現状push

--- SearchRoute.java
+++ SearchRoute.java
@@ -16,24 +16,26 @@ public class SearchRoute{
       System.out.println("To?");
       input = new BufferedReader (new InputStreamReader (System.in));
       start = input.readLine( );
-      System.out.println("入力された駅名は「" + start + "」です。");
+      //System.out.println("入力された駅名は「" + start + "」です。");
       System.out.println("From?");
       input = new BufferedReader (new InputStreamReader (System.in));
       goal = input.readLine( );
-      System.out.println("入力された駅名は「" + goal + "」です。");
+      //System.out.println("入力された駅名は「" + goal + "」です。");
+
       //0.59s br = new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
+
       fr = new FileReader(args[0]); //0.57s
       br = new BufferedReader(fr);
       while (br.ready()) {
         String str = br.readLine(); //1行読込み
         if(row < n){
           station[row] = str;
-          System.out.println(station[row]);
+          // System.out.println(station[row]);
         }else{
           String[]tmp = str.split(" ");
           for(int i=0; i<n; i++){
             data[row-n][i] = Integer.parseInt(tmp[i]);
-            System.out.println(data[row-n][i]);
+            // System.out.println(data[row-n][i]);
            }
         }
         row++;
@@ -41,9 +43,11 @@ public class SearchRoute{
     }catch(IOException e){
       e.printStackTrace();
     }
+    station();
   }
 
   void station(){
+    System.out.println(start);
     //ここにかく
   }
 
